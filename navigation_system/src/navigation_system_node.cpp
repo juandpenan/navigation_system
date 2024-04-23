ea7c797e6ca46ef2a8459820b03499281736bc84
Francisco Martín Rico <fmrico@gmail.com>
2024-04-23 09:10:33 +0200

    Migration to Cascade
    Signed-off-by: Francisco Martín Rico <fmrico@gmail.com>

--- navigation_system/src/navigation_system_node.cpp
+++ navigation_system/src/navigation_system_node.cpp
@@ -19,7 +19,7 @@ int main(int argc, char ** argv)
   rclcpp::init(argc, argv);
   auto node = std::make_shared<navigation_system::NavigationSystem>();
 
-  rclcpp::spin(node);
+  rclcpp::spin(node->get_node_base_interface());
 
   rclcpp::shutdown();
 
